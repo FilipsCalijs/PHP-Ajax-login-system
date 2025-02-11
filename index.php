7ef00df786476ae526eb990f0aed122ad7f4ca3c
FilipsCalijs <filips.calijs@gmail.com>
2025-02-11 10:51:44 +0200

    login page

--- index.php
+++ index.php
@@ -6,8 +6,36 @@
     <title>Complete user registretion and login sistem using php and ajex</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>
-    
+<body class="bg-dark">
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col-lg-4 offset-lg-4" id="alert">
+                <div class="alert alert-success">
+                    <strong id="result"> hello world!</strong>
+
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-4 offset-lg-4 bg-light rounded" id="login-box">
+                    <h2 class="text-center mt-2">Login</h2>
+                    <form action="" method="post" role="from" class="p-2" id="login-frm">
+                        <div class="form-group">
+                            <input type="text" name="username" class="form-control" placeholder="your username" required>
+                        </div>
+                        <div class="form-group">
+                            <input type="password" name="password" class="form-control" placeholder="Password" required>
+                        </div>
+                        <div class="form-group">
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" name="rem" class="custom-control-input" id="customCheck">
+                                <label for="customCheck" class="custom-control-label">Remember Me</label>
+                            </div>
+                        </div>
+                    </form>
+            </div>
+        </div>
+    </div>
 
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
